65105e8c0f6e3bd97ec79b9dccc13e6a98945f49
Meeraj Kumar Sarkar <sarkarmeerajkumar@gmail.com>
2026-09-04 13:14:50 +0530

    adding ninetyninebottles.py

---
+++ ninetyninebottles.py
@@ -0,0 +1,36 @@
+import sys
+import time
+
+print("Ninty-Nine Bottles")
+print()
+print("Press Ctrl-C to QUIT.")
+
+time.sleep(2)
+
+bottles = 99
+PAUSE = 2
+
+try:
+    while bottles > 1:
+        print(bottles, "bottles of milk on the wall,")
+        time.sleep(PAUSE)
+        print(bottles, "bottles of milk,")
+        time.sleep(PAUSE)
+        print("Take one down, pass it around,")
+        time.sleep(PAUSE)
+        bottles -= 1
+        print(bottles, "bottles of milk on the wall!")
+        time.sleep(PAUSE)
+        print()
+
+    print("1 bottle of milk on the wall,")
+    time.sleep(PAUSE)
+    print("1 bottle of milk,")
+    time.sleep(PAUSE)
+    print("Take it down, pass it around,")
+    time.sleep(PAUSE)
+    bottles -= 1
+    print("No more bottles of milk on the wall!")
+    time.sleep(PAUSE)
+except KeyboardInterrupt:
+    sys.exit()
